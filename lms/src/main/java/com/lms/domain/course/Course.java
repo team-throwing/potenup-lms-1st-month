c82b642319f5a2c092da87fbf5ed20a4a9718529
sanseongKo <tkstjd56@naver.com>
2025-10-16 23:54:19 +0900

    [feature/course] 섹션 추가 메서드, 컨텐츠 추가 메서드 추가
    - 컨텐츠 추가는 이후 작업은 섹션에 위임

--- lms/src/main/java/com/lms/domain/course/Course.java
+++ lms/src/main/java/com/lms/domain/course/Course.java
@@ -1,12 +1,16 @@
 package com.lms.domain.course;
 
+import com.lms.domain.course.spec.creation.CreateContent;
 import com.lms.domain.course.spec.creation.CreateCourse;
+import com.lms.domain.course.spec.creation.CreateSection;
 import com.lms.domain.course.spec.rebuild.RebuildCourse;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+
+import static com.lms.domain.course.constvalue.ConstValue.NEXT_SEQ;
 
 public class Course {
     @Getter
@@ -86,9 +90,59 @@ public class Course {
         return List.copyOf(sections);
     }
 
+    public void addSection(CreateSection section) {
+        if (section == null) return;
+
+        Section newSection = Section.create(section);
+
+        /*
+         * 만약 같은 seq가 존재할 경우
+         * 기존의 section의 순번을 1만큼 뒤로 밀고
+         * 기존의 순번 자리에 새로운 섹션을 넣는다.
+         */
+        balanceSeq(newSection);
+        sections.add(Section.create(section));
+
+        touched();
+    }
+
+    private void balanceSeq(Section newSection) {
+        if (isLastSequence(newSection.getSeq())) {
+            newSection.specifiedSeq(getLastSeq());
+        }
+
+        this.sections.stream()
+            .filter(section -> section.getSeq().equals(newSection.getSeq()))
+            .findFirst().ifPresent(Section::nextSeq);
+    }
+
+    public void addContent(CreateContent content, Integer sectionId) {
+        if (content == null) return;
+
+        Content newContent = Content.create(content);
+
+        sections.stream().filter(section -> section.getId().equals(sectionId))
+            .findFirst()
+            .ifPresent(section -> section.addContent(newContent));
+
+        touched();
+    }
+
+    private boolean isLastSequence(Integer seq) {
+        return sections.size() < seq;
+    }
+
+    private Integer getLastSeq() {
+        return sections.size() + NEXT_SEQ;
+    }
+
     private void validateTitle(String title) throws IllegalArgumentException {
         Optional.ofNullable(title).orElseThrow(() ->
             new IllegalArgumentException("강의의 제목이 없습니다. 값을 확인해주세요.")
         );
+    }
+
+    private void touched() {
+        this.updatedAt = LocalDateTime.now();
     }
 }
