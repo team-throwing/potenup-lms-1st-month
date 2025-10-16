7005cd83f661c7b95b89df471e2f4201c4498d71
SeungYeop-Han <ammezkhan@gmail.com>
2025-10-16 09:39:05 +0900

    docs(SqlSyntaxError): 문법 -> 구문

--- lms/src/main/java/com/lms/repository/exception/error/SqlSyntaxError.java
+++ lms/src/main/java/com/lms/repository/exception/error/SqlSyntaxError.java
@@ -1,7 +1,7 @@
 package com.lms.repository.exception.error;
 
 /**
- * SQL 문법 오류
+ * SQL 구문 오류
  */
 public class SqlSyntaxError extends AccessAndGeneralSqlError {
     public SqlSyntaxError(String message, Throwable cause) {
