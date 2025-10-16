b8e642d94fc0f27e5716fefd9edfff0ae6017f4c
SeungYeop-Han <ammezkhan@gmail.com>
2025-10-16 17:46:10 +0900

    feat: CourseRepository 인터페이스 정의
    1. CourseInfoSearchFilter
    : 강좌 정보 검색을 위한 DTO
    
    2. KeywordSearchScope
    : 키워드를 검색할 범위(제목, 제목|개요, 제목|개요|본문)
    
    3. CourseInfo
    : 강좌 정보(섹션 미포함)

---
+++ lms/src/main/java/com/lms/repository/course/CourseRepository.java
@@ -0,0 +1,104 @@
+package com.lms.repository.course;
+
+import com.lms.domain.course.Course;
+import com.lms.repository.course.dto.CourseInfo;
+import com.lms.repository.course.dto.CourseInfoSearchFilter;
+import com.lms.repository.exception.DatabaseException;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CourseRepository {
+
+    /**
+     * @param course 강좌 id
+     * @throws IllegalArgumentException <br/>
+     *      <ul>
+     *          <li>course 가 null</li>
+     *          <li>course.id 가 not null</li>
+     *      </ul>
+     * @throws DatabaseException 복구 가능한 데이터베이스 예외입니다. 예를 들어, <br/>
+     * <ul>
+     *     <li>무결성 제약 조건 위반 (ex. 중복 키 삽입 시도, Not Null 위반, ...)</li>
+     *     <li>잘못된 데이터 형식 (ex. 너무 긴 문자열, 오버 플로우, 언더 플로우, 잘못된 날짜 형식, ...)</li>
+     *     <li>트랜잭션 실패 (ex. 데드락 감지, 락 대기시간 초과, ...)</li>
+     *     <li>등등...</li>
+     * </ul>
+     */
+    void create(Course course);
+
+    /**
+     * @param id 강좌 id
+     * @return 모든 의존성이 채워진 특정한 강좌를 반환
+     * @throws IllegalArgumentException id 가 음수
+     * @throws DatabaseException 복구 가능한 데이터베이스 예외입니다. 예를 들어, <br/>
+     * <ul>
+     *     <li>무결성 제약 조건 위반 (ex. 중복 키 삽입 시도, Not Null 위반, ...)</li>
+     *     <li>잘못된 데이터 형식 (ex. 너무 긴 문자열, 오버 플로우, 언더 플로우, 잘못된 날짜 형식, ...)</li>
+     *     <li>트랜잭션 실패 (ex. 데드락 감지, 락 대기시간 초과, ...)</li>
+     *     <li>등등...</li>
+     * </ul>
+     */
+    Optional<Course> findById(long id);
+
+    /**
+     * @param filter 강좌 검색 필터
+     * @return 페이징된 강좌 검색 결과.
+     * <ul>
+     *     <li>반환 결과 내의 각각의 Course 객체의 sections 는 null 입니다.</li>
+     *     <li>또한 만약 filter 가 `CourseSearchFilter filter = CourseSearchFilter.builder().build()` 와 같이 구현된 경우.
+     *     가장 최근에 수정된 강좌 10 개를 반환합니다.</li>
+     * </ul>
+     * @throws IllegalArgumentException filter 가 null
+     * @throws DatabaseException 복구 가능한 데이터베이스 예외입니다. 예를 들어, <br/>
+     * <ul>
+     *     <li>무결성 제약 조건 위반 (ex. 중복 키 삽입 시도, Not Null 위반, ...)</li>
+     *     <li>잘못된 데이터 형식 (ex. 너무 긴 문자열, 오버 플로우, 언더 플로우, 잘못된 날짜 형식, ...)</li>
+     *     <li>트랜잭션 실패 (ex. 데드락 감지, 락 대기시간 초과, ...)</li>
+     *     <li>등등...</li>
+     * </ul>
+     */
+    List<CourseInfo> searchCourseInfo(CourseInfoSearchFilter filter);
+
+    /**
+     * course 가 존재하지 않는 경우 create 하고,
+     * 존재하는 경우 기존 강좌를 삭제 후 덮어씁니다.
+     * @param course 강좌
+     * @throws IllegalArgumentException course 가 null
+     * @throws DatabaseException 복구 가능한 데이터베이스 예외입니다. 예를 들어, <br/>
+     * <ul>
+     *     <li>무결성 제약 조건 위반 (ex. 중복 키 삽입 시도, Not Null 위반, ...)</li>
+     *     <li>잘못된 데이터 형식 (ex. 너무 긴 문자열, 오버 플로우, 언더 플로우, 잘못된 날짜 형식, ...)</li>
+     *     <li>트랜잭션 실패 (ex. 데드락 감지, 락 대기시간 초과, ...)</li>
+     *     <li>등등...</li>
+     * </ul>
+     */
+    void replace(Course course);
+
+    /**
+     * 변경된 부분만 수정합니다.
+     * @param course 강좌
+     * @throws IllegalArgumentException course 가 null
+     * @throws DatabaseException 복구 가능한 데이터베이스 예외입니다. 예를 들어, <br/>
+     * <ul>
+     *     <li>무결성 제약 조건 위반 (ex. 중복 키 삽입 시도, Not Null 위반, ...)</li>
+     *     <li>잘못된 데이터 형식 (ex. 너무 긴 문자열, 오버 플로우, 언더 플로우, 잘못된 날짜 형식, ...)</li>
+     *     <li>트랜잭션 실패 (ex. 데드락 감지, 락 대기시간 초과, ...)</li>
+     *     <li>등등...</li>
+     * </ul>
+     */
+    void update(Course course);
+
+    /**
+     * @param id 강좌 id
+     * @throws IllegalArgumentException id 가 음수
+     * @throws DatabaseException 복구 가능한 데이터베이스 예외입니다. 예를 들어, <br/>
+     * <ul>
+     *     <li>무결성 제약 조건 위반 (ex. 중복 키 삽입 시도, Not Null 위반, ...)</li>
+     *     <li>잘못된 데이터 형식 (ex. 너무 긴 문자열, 오버 플로우, 언더 플로우, 잘못된 날짜 형식, ...)</li>
+     *     <li>트랜잭션 실패 (ex. 데드락 감지, 락 대기시간 초과, ...)</li>
+     *     <li>등등...</li>
+     * </ul>
+     */
+    void delete(long id);
+}
