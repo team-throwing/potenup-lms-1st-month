2c927cfb80ab8f01b0b712f8db79fed9cf4c81ff
minji0828 <entersuk1998@naver.com>
2025-10-16 12:14:29 +0900

    public부분 private로 수정

--- lms/src/main/java/com/lms/domain/category/Category.java
+++ lms/src/main/java/com/lms/domain/category/Category.java
@@ -21,13 +21,13 @@ public class Category {
         this.parentId = parentId;
     }
 
-    public Category create(
+    private Category create(
         String name, CategoryLevel level, Integer parentId
     ) {
         return new Category(null, name, level, parentId);
     }
 
-    public Category rebuild(
+    private Category rebuild(
         Integer id, String name, CategoryLevel level, Integer parentId
     ) {
         return new Category(id, name, level, parentId);
