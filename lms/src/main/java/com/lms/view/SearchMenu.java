40951e4da181ea9e6758da73d2f9edefcbb737b7
JinYong833 <jjystudy1@gmail.com>
2025-10-19 11:54:55 +0900

    조회 메뉴에는 코스를 검색하여 코스 메뉴로 진입하는 방법과 카테고리를 검색하여 코스 메뉴에 진입하는 방법이 있음

--- lms/src/main/java/com/lms/view/SearchMenu.java
+++ lms/src/main/java/com/lms/view/SearchMenu.java
@@ -1,124 +1,45 @@
 package com.lms.view;
 
-import com.lms.domain.category.CategoryLevel;
-
 import java.util.Scanner;
 
 public class SearchMenu {
-    Scanner scanner = new Scanner(System.in);
+    private final CategoryManagement  categoryManagement;
+//    private final CourseManagement  courseManagement;
 
-    public void showSearchMenu() {
-//        CategoryService categoryService = new CategoryService();
-//        CourseService courseService = new CourseService();
+    public SearchMenu(CategoryManagement categoryManagement) {
+        this.categoryManagement = categoryManagement;
+    }
 
-        CategoryLevel categoryLevel = CategoryLevel.ONE;
+//    public SearchMenu(CategoryManagement categoryManagement, CourseManagement courseManagement) {
+//        this.categoryManagement = categoryManagement;
+//        this.courseManagement = courseManagement;
+//    }
+
+    public void showSearchMenu(Scanner scanner) {
 
         while (true) {
-        // 최상위 카테고리 목록 출력
-        System.out.println("1. 카테고리 목록 보기");
+            // 최상위 카테고리 목록 출력
+            System.out.println("1. 카테고리 조회");
+            System.out.println("2. 강좌 검색");
+            System.out.println("3. 뒤로가기");
 
-        System.out.println("2. 강좌 검색");
-
-        int userInput = scanner.nextInt();
-        scanner.nextLine();
-
-        switch (userInput) {
-            case 1:
-                System.out.println("=== 상위 카테고리 목록 ===");
-//                List<Category> categories1 = categoryService.findAllByCategoryLevel(categoryLevel);
-//                for (Category category : categories1) {
-//                    System.out.println("ID: " + category.getId() + "Name: " + category.getName());
-//                }
-
-                System.out.println("=== 카테고리 선택 ===");
-                System.out.println("취소하려면 0을 입력하세요.");
-                System.out.print("카테고리 선택(id): ");
-
-                int inputCategoryId1 = scanner.nextInt();
-                scanner.nextLine();
-
-                if (inputCategoryId1 == 0) {
-                    System.out.println("카테고리 선택을 취소합니다.");
-                    break;
-                }
-
-                System.out.println("\n=== 하위 카테고리 목록 ===");
-//                List<Category> categories2 = categoryService.findAllByCategoryLevel(CategoryLevel.TWO);
-//                for (Category category : categories2) {
-//                    System.out.println("ID: " + category.getId() + "Name: " + category.getName());
-//                }
-
-                System.out.println("=== 카테고리 선택 ===");
-                System.out.println("취소하려면 0을 입력하세요.");
-                System.out.print("카테고리 선택(id): ");
-
-                int inputCategoryId2 = scanner.nextInt();
-                scanner.nextLine();
-
-                if (inputCategoryId2 == 0) {
-                    System.out.println("카테고리 입력을 취소합니다.");
-                    break;
-                }
-
-                System.out.println("=== " + inputCategoryId2 + "카테고리의 코스 목록 출력 ===");
-//                List<Course> categoryCourses = courseService.findByCategoryId(inputCategoryId2);
-//                for (Course course : categoryCourses) {
-//                    System.out.println("ID: " + course.getId() + "title: " + course.getTitle());
-//                }
-//                break;
-
-            System.out.println("=== 코스 선택 ===");
-            System.out.println("취소하려면 0을 입력하세요.");
-            System.out.println("코스 선택(id): ");
-
-            int inputCourseId = scanner.nextInt();
+            int userInput = scanner.nextInt();
             scanner.nextLine();
 
-            if (inputCourseId == 0) {
-                System.out.println("코스 선택 취소");
-                break;
-            }
-
-            new CourseManagement().showCourseMenu(inputCourseId);
-            case 2:
-                System.out.println("=== 강좌 검색 ===");
-                System.out.println("취소하려면 0을 입력하세요.");
-                System.out.print("강좌 검색(제목): ");
-                String keyword = scanner.nextLine();
-
-                if (keyword.equals("0")) {
-                    System.out.println("강좌 검색을 취소합니다.");
+            switch (userInput) {
+                case 1:
+                    categoryManagement.showCategoryLevelOne(scanner);   // 상위 카테고리 목록 조회
+                    categoryManagement.showCategoryLevelTwo(scanner);   // 부모 카테고리 ID로 자식 카테고리 목록 조회
+//                    courseManagement.showCourseByCategoryId(scanner); // 자식 카테고리 ID에 있는 강좌 목록 조회
+//                    courseManagement.showCourseMenu(scanner);         // 강좌 id를 입력해서 해당 코스 메뉴로 진입
                     break;
-                }
-
-//                List<Course> courses = CourseService.searchCourseInfo();
-//                for (Course course : courses) {
-//                    System.out.println("ID: " + course.getId() + "title: " + course.getTitle());
-//                }
-
-                System.out.println("\n=== 코스 선택 ===");
-                System.out.println("취소하려면 0을 입력하세요.");
-
-                int searchCourseId = scanner.nextInt();
-                scanner.nextLine();
-
-                if (searchCourseId == 0) {
-                    System.out.println("코스 선택을 취소합니다.");
+                case 2:
+//                    courseManagement.showCourseByKeyword(scanner);    // 강좌 검색 후 관련 강좌 목록 조회
+//                    courseManagement.showCourseMenu(scanner);         // 강좌 id를 입력해서 해당 강좌 메뉴로 진입
                     break;
-                }
-
-                new CourseManagement().showCourseMenu(searchCourseId);
-                break;
+                case 3:
+                    return;
             }
         }
-    }
-
-
-    public void showFirstCategory() {
-
-    }
-
-    public void showSecondCategory() {
-
     }
 }
