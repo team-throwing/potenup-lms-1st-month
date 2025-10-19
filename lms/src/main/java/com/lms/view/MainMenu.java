9f0765ae530835642cb84f7660fc975a97e4afd7
JinYong833 <jjystudy1@gmail.com>
2025-10-19 11:49:06 +0900

    MainMenu에 searchMenu 의존성 추가

--- lms/src/main/java/com/lms/view/MainMenu.java
+++ lms/src/main/java/com/lms/view/MainMenu.java
@@ -1,24 +1,26 @@
 package com.lms.view;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class MainMenu {
 
-    private final CategoryManu categoryManu;
-
-    public MainMenu(CategoryManu categoryManu) {
-        this.categoryManu = categoryManu;
+    private final CategoryMenu categoryMenu;
+    private final SearchMenu searchMenu;
+    public MainMenu(CategoryMenu categoryMenu, SearchMenu searchMenu) {
+        this.categoryMenu = categoryMenu;
+        this.searchMenu = searchMenu;
     }
 
 
-    public void showMainMenu(Scanner scanner) {
+    public void showMainMenu(Scanner scanner) throws SQLException {
         System.out.println("\nTeam Janban 1st_lms_project");
         while (true) {
             System.out.println("\n=== 메인 메뉴 ===");
 
             // 강좌 및 카테고리 조회 메뉴
             System.out.println("1. 강좌/카테고리 조회 메뉴");
-            System.out.println("2. 관리자 메뉴");
+            System.out.println("2. 관리자 메뉴");        // 카테고리 CRUD 메뉴
             System.out.println("3. 프로그램 종료");
             System.out.print("메뉴 선택: ");
 
@@ -27,10 +29,12 @@ public class MainMenu {
 
             switch (userInput) {
                 case 1:
-                    new SearchMenu().showSearchMenu();
+                    // 조회 메뉴 진입
+                    searchMenu.showSearchMenu(scanner);
                     break;
                 case 2:
-                    categoryManu.showManagementMenu(scanner);
+                    // 카테고리 관리 메뉴 진입
+                    categoryMenu.showCategoryManagementMenu(scanner);
                     break;
                 case 3:
                     System.exit(0);
