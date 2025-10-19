df8a89b7ecf12eb96da268d3073502a214c0527a
JinYong833 <jjystudy1@gmail.com>
2025-10-19 11:47:50 +0900

    Main에 throw Exception 추가

--- lms/src/main/java/com/lms/Main.java
+++ lms/src/main/java/com/lms/Main.java
@@ -6,23 +6,26 @@ import com.lms.repository.exception.converter.DbDialectExceptionConverter;
 import com.lms.repository.exception.converter.DbUtils;
 import com.lms.repository.exception.converter.MysqlExceptionConverter;
 import com.lms.service.CategoryService;
-import com.lms.view.CategoryManagement;
-import com.lms.view.MainMenu;
-import com.lms.view.CategoryManu;
+import com.lms.view.*;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Scanner sc = new Scanner(System.in);
         DbDialectExceptionConverter exceptionConverter = new MysqlExceptionConverter();
         DbUtils dbUtils = new DbUtils(exceptionConverter);
         CategoryRepository categoryRepository = new CategoryRepositoryImpl(dbUtils);
+//        CourseRepository courseRepository = new CourseRepositoryImpl(dbUtils);
         CategoryService categoryService = new CategoryService(categoryRepository);
+//        CourseService courseService = new CourseService(courseRepository);
         CategoryManagement categoryManagement = new CategoryManagement(categoryService);
-        CategoryManu categoryManu = new CategoryManu(categoryManagement);
-//        SearchMenu searchMenu = new SearchMenu(categoryService, courseService);
-        MainMenu mainMenu = new MainMenu(categoryManu);
+//        CourseManagement courseManagement = new CourseManagement(courseService);
+        CategoryMenu categoryMenu = new CategoryMenu(categoryManagement);
+//        CourseManu courseManu = new CourseManu(courseManagement);
+        SearchMenu searchMenu = new SearchMenu(categoryManagement); // courseManagement 추가해야함
+        MainMenu mainMenu = new MainMenu(categoryMenu, searchMenu);
 
         mainMenu.showMainMenu(sc);
     }
